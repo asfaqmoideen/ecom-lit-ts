88455dc533fc27240c5b249513c60eb4b274b8c7
asfaqmoideen <asfaqmoideen@gmail.com>
2025-03-18 12:09:24 +0530

    Merge branch 'build-RoutingLitComponents'

--- src/services/APIService.ts
+++ src/services/APIService.ts
@@ -1,55 +1,44 @@
 export class APIService{
 
-    private baseURL : string = "https://dummyjson.com/products";
-    
-      async tryFetchingData(endpoint :string){
-        try {
-          const response = await fetch(`${this.baseURL}/${endpoint}`, { method: "GET" });
-          
-          if (!response.ok) {
-            throw new Error("Unable to Fetch");
-          }
-          return response.json();
-        } catch (e) {
-          console.log(e);
+  private baseURL : string = "https://dummyjson.com/products";
+  
+    async tryFetchingData(endpoint :string){
+      try {
+        const response = await fetch(`${this.baseURL}/${endpoint}`, { method: "GET" });
+        
+        if (!response.ok) {
+          throw new Error("Unable to Fetch");
         }
+        return response.json();
+      } catch (e) {
+        console.log(e);
       }
+    }
+  
+    async getAllProducts() {
+      return await this.tryFetchingData('');
+    }
+  
+  
+    async searchProduct(productname :string) {
+      return this.tryFetchingData(`search?q=${productname}`);
+    }
+  
+    async getProducts(limit :number, skip:number){
+      return await this.tryFetchingData(`?limit=${limit}&skip=${skip}`)
+    }
+
+    async getProductCategories(){
+      return await this.tryFetchingData(`category-list`)
+    }
+
+    async getProductsByCategory(category :string) {
+      return await this.tryFetchingData(`category/${category}`)
+    }
+
+    async getProductById(productID :number){
+      return await this.tryFetchingData(`${productID}`)
+    }
+
     
-      async getAllProducts() {
-        return await this.tryFetchingData('');
-      }
-    
-      async sortProducts(value :number, order :number) {
-        return await this.tryFetchingData(
-          `?sortBy=${value}&order=${order}`
-        );
-      }
-
-      async filterProducts(filter :number, value :number) {
-        return await this.tryFetchingData(
-          `${this.baseURL}/filter?key=${filter}&value=${value}`
-        );
-      }
-    
-      async searchProduct(productname :string) {
-        return this.tryFetchingData(`${this.baseURL}/search?q=${productname}`);
-      }
-    
-      async getProducts(limit :number, skip:number){
-        return await this.tryFetchingData(`?limit=${limit}&skip=${skip}`)
-      }
-
-      async getProductCategories(){
-        return await this.tryFetchingData(`category-list`)
-      }
-
-      async getProductsByCategory(category :string) {
-        return await this.tryFetchingData(`category/${category}`)
-      }
-
-      async getProductById(productID :number){
-        return await this.tryFetchingData(`${productID}`)
-      }
-
-      
 }
